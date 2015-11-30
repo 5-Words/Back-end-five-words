7c578b3aaf2aa10b2200c486e9b43a01326a0c1a
Charles <codprestige10@gmail.com>
2015-11-30 10:55:57 -0500

    initial commit, signup, login, and delete user working locally

---
+++ five_words/app/controllers/registrations_controller.rb
@@ -0,0 +1,44 @@
+class RegistrationsController < ApplicationController
+
+	def index
+		render plain: "Hello World"
+	end
+
+	def create
+		@user = User.new(email: params[:email],
+						username: params[:username],
+						password: params[:password],
+						name: params[:name])
+		if @user.save
+			render "create.json.jbuilder", status: :created
+		else
+			render json: {errors: @user.errors.full_messages },
+			status: :unprocessable_entity
+		end
+	end
+
+
+	def login
+
+		@user = User.find_by(username: params[:username])
+		if @user && @user.authenticate(params[:password])
+			binding.pry
+			render "login.json.jbuilder", status: :ok
+		else
+			render json: {error: "Could not find user for #{params[:username]} or wrong password." },
+			status: :unauthorized
+		end
+	end
+
+	def destroy
+		@user = User.find_by(username: params[:username])
+		if @user && @user.authenticate(params[:password])
+			@user.destroy
+			render plain: "User has been deleted"
+		else
+			render json: { error: "Invalid email (#{params[:email]}) or password"},
+			status: :unauthorized
+		end
+	end
+
+end
