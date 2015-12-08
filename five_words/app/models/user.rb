9cbea88383ac8a6b3d85b7f23acb86e4918042fb
Charles <codprestige10@gmail.com>
2015-12-08 12:15:30 -0500

    debugging server

--- five_words/app/models/user.rb
+++ five_words/app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
 	has_many :words
 	has_secure_password
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
-  
+
 
 
 
