037e0092314172fbaaeff5f4d5bbb683141e857c
Charles <codprestige10@gmail.com>
2015-12-09 11:37:39 -0500

    still working on matches

--- five_words/app/controllers/words_controller.rb
+++ five_words/app/controllers/words_controller.rb
@@ -51,10 +51,10 @@ class WordsController < ApplicationController
 	def user_words
 		words = current_user.words.where(category: params[:category])
 		
-		@matches = Word.where(category: params[:category],
+		@matches = Word.where(
 													word: [words[0]["word"], words[1]["word"],
 																 words[2]["word"], words[3]["word"],
-																 words[4]["word"]]).where.not(user_id: current_user.id)
+																 words[4]["word"]]).where.not(user_id: current_user.id).where(category: params[:category])
 	  # ver 1: get the user for each match
 		# ver 2: group/order them by user
 		# we might consider eager loading associations
